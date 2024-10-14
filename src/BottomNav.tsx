7a37b43c651f480b5795cd3b84a8f218eb0ebe96
bitshiftmod <bitshiftmod@gmail.com>
2024-10-14 14:36:12 -0400

    chore: fix imports

--- src/BottomNav.tsx
+++ src/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigation, useRoutes } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface NavButtonProps {
   selectedIndex: number;
