9077cbd50e31b6ad7b609072a379b05d9b376d3c
bitshiftmod <bitshiftmod@gmail.com>
2024-10-31 01:08:14 -0400

    feat: reworked window placement using plugin-positioner and add Show Window to menu to accomodate linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use tauri::menu::{Menu, MenuItem};
 use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
 use tauri::Manager;
+use tauri_plugin_positioner::{Position, WindowExt};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -10,18 +11,28 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_log::Builder::new().build())
         .setup(|app| {
+            let show_window =
+                MenuItem::with_id(app, "showWindow", "Show Window", true, None::<&str>)?;
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i])?;
+            let menu = Menu::with_items(app, &[&show_window, &quit_i])?;
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .icon_as_template(true)
                 .menu(&menu)
                 .menu_on_left_click(false)
                 .on_menu_event(|app, event| match event.id().as_ref() {
+                    "showWindow" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.as_ref().window().move_window(Position::TrayCenter);
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
                     "quit" => {
                         println!("quit menu item was clicked");
                         app.exit(0);
@@ -30,37 +41,24 @@ pub fn run() {
                         println!("menu item {:?} not handled", event.id());
                     }
                 })
-                .on_tray_icon_event(|tray, event| match event {
-                    TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        position,
-                        ..
-                    } => {
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let size = window.outer_size().unwrap();
-                            // println!("outer size: {:?}", size);
+                .on_tray_icon_event(|tray_handle, event| {
+                    tauri_plugin_positioner::on_tray_event(tray_handle.app_handle(), &event);
 
-                            let y_position = if position.y < (size.height as f64 / 2.0) {
-                                0
-                            } else {
-                                (position.y - size.height as f64 - 20.0) as i32
-                            };
-
-                            let _ = window.set_position(tauri::Position::Physical(
-                                tauri::PhysicalPosition {
-                                    x: (position.x as f64 - size.width as f64 / 2.0) as i32,
-                                    y: y_position,
-                                },
-                            ));
-
-                            let _ = window.show();
-                            let _ = window.set_focus();
+                    match event {
+                        TrayIconEvent::Click {
+                            button: MouseButton::Left,
+                            button_state: MouseButtonState::Up,
+                            ..
+                        } => {
+                            let app = tray_handle.app_handle();
+                            if let Some(window) = app.get_webview_window("main") {
+                                let _ = window.as_ref().window().move_window(Position::TrayCenter);
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
                         }
-                    }
-                    _ => {
-                        // println!("unhandled event {event:?}");
+
+                        _ => {}
                     }
                 })
                 .build(app)?;
